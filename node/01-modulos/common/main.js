fcff9a0e8064b054d5c2df66ac7da542a2c53bf0
Eduardo Marques <eduardomar1703@gmail.com>
2024-10-24 18:39:57 -0300

    add

--- node/01-modulos/common/main.js
+++
@@ -1,5 +0,0 @@
-// importando no padrão commonjs
-const {sum, subtract} = require('./functions');
-
-console.log(sum(17, 8));
-console.log(subtract(21, 18));
--- node/01-modulos/esm/functions.mjs
+++
@@ -1,7 +0,0 @@
-export const multiply = (a, b) => {
-    return a * b;
-}
-
-export const division = (a, b) => {
-    return a / b;
-}
